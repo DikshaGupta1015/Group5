0d8f5276acc3f58c179e89afb199390dbb6bfa73
DikshaGupta1015 <34881041+DikshaGupta1015@users.noreply.github.com>
2018-01-02 15:51:41 +0530

    Add project files.

---
+++ MVCDay4_AttributeRouting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDay4_AttributeRouting.MvcApplication" Language="C#" %>
---
+++ MVCDay4_AttributeRouting/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDay4_AttributeRouting.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
